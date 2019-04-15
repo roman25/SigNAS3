df0440b2724cb52c5b40a4dae73b836572b87281
roman25 <ro25man@yandex.ru>
2019-04-15 18:53:00 +0300

    Request #1

--- main.cpp
+++ main.cpp
@@ -18,23 +18,32 @@ int main(int argc, char *argv[])
 
     QDate currDate = QDate::currentDate();
     QTime currTime = QTime::currentTime();
-    QString path_t = currDate.toString("dd_MM_yy_")+currTime.toString("hh_mm");
-    i.pathToReport = "./result_"+path_t+".csv";
+    QString path_t = currDate.toString("dd_MM_yy_") + currTime.toString("hh_mm");
+    i.pathToReport = "./result_" + path_t + ".csv";
     i.pathToRST = "./script0.rst";
     i.pathToCSV = "./res.csv";
 
-    if(argc>0){
-        i.idOriginal = QString(argv[1]);
-        i.srOriginal = QString(argv[2]);
+    if (argc >= 7) {
+        i.pathToRST = QString(argv[1]);
+        i.pathToCSV = QString(argv[2]);
+        i.pathToReport = QString(argv[3]);
+        i.idOriginal = QString(argv[4]);
+        i.srOriginal = QString(argv[5]);
         bool ok;
-        i.k = QString(argv[3]).toInt(&ok);
-        i.maxBB = QString(argv[4]).toInt(&ok);
+        i.k = QString(argv[6]).toInt(&ok);
+        i.maxBB = QString(argv[7]).toInt(&ok);
+    }
+    else {
+        qInfo() << "Enter all 7 parameters: pathtoRST, pathtoCSV, pathtoReport, idOriginl, srOriginal, k, maxBB";
+        return -1;
     }
 
-    parseResults *parser = new parseResults(i);
+    parseResults *parser = new parseResults(i.pathToReport, i.pathToRST, i.pathToCSV, i.idOriginal, i.srOriginal, i.k, i.maxBB);
+
     parser->analyzeCSV();
     parser->analyzeRST();
 
     delete parser;
+
     return 0;
 }
